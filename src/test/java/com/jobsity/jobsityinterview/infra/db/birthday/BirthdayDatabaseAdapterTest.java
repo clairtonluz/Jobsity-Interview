c885ca0ee45af81bf466b60f101f009460ba5afb
Clairton Luz <clairton.c.l@gmail.com>
2023-04-02 13:27:29 -0300

    increasing test coverage

--- src/test/java/com/jobsity/jobsityinterview/infra/db/birthday/BirthdayDatabaseAdapterTest.java
+++ src/test/java/com/jobsity/jobsityinterview/infra/db/birthday/BirthdayDatabaseAdapterTest.java
@@ -1,26 +1,21 @@
 package com.jobsity.jobsityinterview.infra.db.birthday;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Paths;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BirthdayDatabaseAdapterTest {
 
     private BirthdayDatabaseAdapter birthdayDatabasePort;
 
-    @BeforeEach
-    void setUp() {
-        var path = Paths.get("src/test/resources/birthdays.txt").toAbsolutePath().toString();
-        birthdayDatabasePort = new BirthdayDatabaseAdapter(path);
-    }
-
     @Test
     void findAll() {
+        var path = Paths.get("src/test/resources/birthdays.txt").toAbsolutePath().toString();
+        birthdayDatabasePort = new BirthdayDatabaseAdapter(path);
+
         var birthdayList = birthdayDatabasePort.findAll();
         assertNotNull(birthdayList);
         assertEquals(3, birthdayList.size());
@@ -29,5 +24,13 @@ class BirthdayDatabaseAdapterTest {
         assertEquals("John", fisrtBirthday.getFirstName());
         assertEquals(LocalDate.of(1982, 10, 8), fisrtBirthday.getDateOfBirth());
         assertEquals("john.doe@foobar.com", fisrtBirthday.getEmail());
+    }
+
+
+    @Test
+    void fileNotFound() {
+        var path = Paths.get("src/test/resources/birthdays-fake.txt").toAbsolutePath().toString();
+        birthdayDatabasePort = new BirthdayDatabaseAdapter(path);
+        assertThrows(RuntimeException.class, () -> birthdayDatabasePort.findAll());
     }
 }
